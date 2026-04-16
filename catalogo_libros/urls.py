e24295646093a674ab64d0865a8f16d9a0516418
Xavier Duran <xavierduran407@gmail.com>
2026-04-16 15:24:32 -0400

    Mostrar lista de libros en la pagina web

--- catalogo_libros/urls.py
+++ catalogo_libros/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from catalogo_libros import views
 
 urlpatterns = [
-    path('', views.listaLibros, name='lista_libros'),
+    path('', views.listaLibros, name='libros'),
     path('libros/<int:id>', views.detalle_libro, name='detalle_libro'),
 ]
